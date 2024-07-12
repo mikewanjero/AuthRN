cd752d7b11bd974b24bcc7edbdb76648e60a0235
mikewanjero <mikewanj@gmail.com>
2024-07-12 12:10:42 +0300

    Logging users in

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,7 +1,24 @@
-import AuthContent from '../components/Auth/AuthContent';
+import { useState } from "react";
+
+import AuthContent from "../components/Auth/AuthContent";
+import LoadingOverlay from "../components/ui/LoadingOverlay";
+import { login } from "../util/auth";
 
 function LoginScreen() {
-  return <AuthContent isLogin />;
+  const [isAuthenticating, setIsAuthenticating] = useState(false);
+
+  //function called, user authenticated, created, authentication removed for next session
+  async function loginHandler({ email, password }) {
+    setIsAuthenticating(true);
+    await login(email, password);
+    setIsAuthenticating(false);
+  }
+
+  if (isAuthenticating) {
+    return <LoadingOverlay message="Logging in...." />;
+  }
+
+  return <AuthContent isLogin onAuthenticate={loginHandler} />;
 }
 
 export default LoginScreen;
